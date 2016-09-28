115c323eecbfee874e6057a902bfd14cf900b14d
weswhitney <wesleywhitney@gmail.com>
2016-09-27 21:07:37 -0600

    delete and check function working

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,9 +2,8 @@ import { Template } from 'meteor/templating';
 
 import { Tasks } from '../api/tasks.js';
 
+import './task.js';
 import './body.html';
-
-import './tasks.html';
 
 Template.body.helpers({
   tasks() {
