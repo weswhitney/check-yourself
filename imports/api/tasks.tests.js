4657ce03812a804265ff5b7eaa253cd9ac91595c
weswhitney <wesleywhitney@gmail.com>
2016-10-17 20:21:20 -0600

    before each tester seed

--- imports/api/tasks.tests.js
+++ imports/api/tasks.tests.js
@@ -8,6 +8,19 @@ import { Tasks } from './tasks.js';
 if (Meteor.isServer) {
   describe('Tasks', () => {
     describe('methods', () => {
+      const userId = Random.id();
+      let taskId;
+
+      beforeEach(() => {
+        Tasks.remove({});
+        taskId = Tasks.insert({
+          text: 'test task',
+          createdAt: new Date(),
+          owner: userId,
+          username: 'tmeasday',
+        });
+      });
+
       it('can delete owned task', () => {
       });
     });
